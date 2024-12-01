5ff70d80073ba4ddd444d21f1ed26b780ee0d775
RicardoRpm <Ricardomiguel190@gmail.com>
2024-12-01 00:10:39 +0000

    first

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace chatApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace chatApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
